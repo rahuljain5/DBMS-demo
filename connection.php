447cc90ed1874251ccd865c66bb295b9306f45ce
rahuljain5 <rjain.rahul5@gmail.com>
2018-06-29 17:04:04 +0530

    Minor Fix

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
 <?php
 //TO ESTABLISH A CONNECTION
-$connect=mysqli_connect('HOSTNAME_HERE','USERNAME_HERE','PASSWORD_HERE','DATABASENAME_HERE');
+$connect = mysqli_connect('HOSTNAME_HERE', 'USERNAME_HERE', 'PASSWORD_HERE', 'DATABASENAME_HERE');
 //ERROR IN ESTABLISHING A CONNECTION
-if(mysqli_connect_errno($connect))
-{
-echo 'failed to connect';
+if (mysqli_connect_errno($connect)) {
+    echo 'failed to connect';
 }
 ?>
